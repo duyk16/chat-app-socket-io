26f648daa6e58ececcbfca6fcaa259685fa4f2a3
Monkey <duyk16@gmail.com>
2019-02-21 23:28:38 +0700

    using react and add login state

--- app.js
+++ app.js
@@ -11,12 +11,33 @@ app.get('/', (req, res) => {
   res.sendFile('/index.html')
 })
 
+var userOnline = {
+  "Bill Gate": "socketId1"
+}
 io.on('connection', function (socket) {
   console.log(socket.id, 'connected');
 
   socket.on('CLIENT-DATA', (data) => {
     console.log(data);
   })
+
+  socket.on('REGISTER', (data) => {
+    if (userOnline[data]) {
+      socket.emit('REGISTER', {status: 'error'})
+      return
+    }
+    
+    userOnline[data] = socket.id
+    console.log(userOnline);
+    
+    socket.emit('REGISTER', {status: 'success', data})
+    io.emit('USER-ONLINE', {data: userOnline})
+  })
+
+    io.emit('USER-ONLINE', {
+      data: userOnline
+    })
+
 });
 
 http.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,15 +1,206 @@
 const socket = io();
 
-$(document).ready(function() {
-  $('#sent-message').on('click', function() {
-    let textInput = $('#text-input').val()
-    if (!textInput) return
-    
-    socket.emit('CLIENT-DATA', {
-      userName,
-      textInput
-    })
+// $(document).ready(function() {
+//   // $('#wrapper').hide()
 
-  })
+//   $('#submit-register').on('click', function() {
+//     let userName = $('#user-name-register').val()
+//     if (!userName) return 
+//     socket.emit('REGISTER', userName)
+//   })
+
+//   socket.on('REGISTER', (res) => {
+//     if (res.status === 'success') {
+//       $('#register').hide()
+//       $('#wrapper').show()
+//     }
+//   })
+//   socket.on('USER-ONLINE', (res) => {
+//     let data = res.data
+//     let text = ''
+//     for (let userName in data) {
+//       let item = `<div className="list-user-item" data-id="${data[userName]}">
+//             <div className="user-name">${userName}</div>
+//             <i className="fas fa-circle text-green"></i>
+//             <span className="user-status">Online</span>
+//           </div>`
+//       text += item
+//     }
+//     $('#user-online').append(text)
+//   })
+
+
+
+//   $('#sent-message').on('click', function() {
+//     let textInput = $('#text-input').val()
+//     if (!textInput) return
+    
+//     socket.emit('CLIENT-DATA', textInput)
+
+//   })
   
-})
+// })
+
+class App extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      userName: ''
+    }
+
+    this._register = this._register.bind(this)
+  }  
+
+  _register(userName) {
+    if (!userName) return
+    socket.emit('REGISTER', userName)
+  }
+
+  componentDidMount = () => {
+    socket.on('REGISTER', (res) => {
+      if (res.status === 'success') {
+        console.log(res);
+        this.setState({
+          ...this.state,
+          userName: res.data
+        })
+      }
+    })
+  };
+  
+
+  render() {
+    return (
+      <div>
+        {!this.state.userName ? 
+          <Register 
+            register={this._register}
+          /> :
+          <Main />
+        }
+      </div>
+    )
+  }
+}
+
+class Register extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      userName: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+  
+
+  handleChange(e) {
+    let userName = e.target.value;
+    this.setState({
+      userName
+    })
+  }
+  render() {
+    return (
+      <div id="register" className="register-group">
+        <div className="register-item">
+          <input 
+            type="text" 
+            id="user-name-register" 
+            placeholder="Register your name" 
+            maxlength="20" 
+            onChange={this.handleChange} 
+            onKeyUp={(e) => {
+              if (e.keyCode === 13) {
+                this.props.register(this.state.userName)
+              }
+            }}
+          />
+        </div>
+        <div className="register-item">
+          <button 
+            id="submit-register" 
+            onClick={() => this.props.register(this.state.userName)}
+          >
+            <i className="fas fa-arrow-right"></i>
+          </button>
+        </div>
+      </div>
+    ) 
+  }
+}
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div id="wrapper">
+
+        <div className="col-4">
+          <div id="status-aside">
+            <div className="scroll" id="user-online">
+
+              <UserStatusItem />
+
+            </div>
+          </div>
+        </div>
+
+        <div className="col-8">
+          <div id="main">
+            <div id="content">
+              <ul>
+
+                <li>
+                  <div className="message item-left">Hello</div>
+                  <div style={{clear: "both"}}></div>
+                </li>
+                {/* <li>
+                  <div className="message item-right">Hello</div>
+                  <div style="clear: both"></div>
+                </li>
+                <li>
+                  <div className="message item-right">Hello</div>
+                  <div style="clear: both"></div>
+                </li>
+                <li>
+                  <div className="message item-left">Hello</div>
+                  <div style="clear: both"></div>
+                </li> */}
+            
+              </ul>
+            </div>
+
+            <div id="chat-input">
+              <div className="input-item">
+                <textarea 
+                  id="text-input" 
+                  placeholder="Type your message..."
+                >
+
+                </textarea>
+              </div>
+
+              <div className="button" id="sent-message">
+                <i className="fab fa-telegram-plane"></i>
+              </div>
+
+            </div>
+          </div>        
+        </div>
+        
+      </div>
+    )
+  }
+}
+class UserStatusItem extends React.Component {
+  render() {
+    return (
+      <div className="list-user-item item-active">
+        <div className="user-name">Anhxtanh</div>
+        <i className="fas fa-circle text-green"></i>
+        <span className="user-status"> Online</span>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
